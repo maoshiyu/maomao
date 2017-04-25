6ca689128fad6db661451a1055057fda88cbb16d
夜色▎  零° <1036661016@qq.com>
2017-04-25 21:06:43 +0800

    shangchuan

---
+++ src/main/java/bwei/com/butterknife2/MainActivity.java
@@ -0,0 +1,31 @@
+package bwei.com.butterknife2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.tx1)
+    TextView tx1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+    }
+
+
+    @OnClick(R.id.tx1)
+    public void onViewClicked() {
+    }
+}
